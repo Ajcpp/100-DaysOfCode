eb3f8807c475de177cf52f24e1cb3a6a181cd86f
Ankush Jha <44066808+Ajcpp@users.noreply.github.com>
2023-04-24 10:31:13 +0530

    inputoutputstring.java

---
+++ Day_6/inputoutputstring.java
@@ -0,0 +1,28 @@
+//String
+//Declaring and Printing
+
+import java.util.Scanner;
+
+public class Day6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //Declaring a string
+        String str="Hello there";
+        String str2=new String();
+        System.out.println("Enter other String");
+        sc.nextLine();//can take any string ,character, number, symbols etc.
+        //Printing a String
+        System.out.println(str);
+        System.out.println(str2);
+
+
+
+    }
+}
+//Test cases
+//for str
+//it print "Hello there"
+//for str2
+//it take the input from user
+//ex:: I am Ankush
+//then it prints "Ankush"
